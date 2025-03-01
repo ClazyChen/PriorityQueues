cf53023c7261f49a03bb22e4a1ab3fa578b7a182
Burry <mfsang@EthanSang-2.local>
2025-03-01 20:54:58 +0800

    Updated by EthanSang: 20250301

--- src/main/scala/ShiftRegister/ShiftRegister.scala
+++ src/main/scala/ShiftRegister/ShiftRegister.scala
@@ -3,17 +3,50 @@ package ShiftRegister
 import chisel3._
 import chisel3.util._
 
-class ShiftRegister(width: Int) extends Module {
+/** ShiftRegister
+ * @param n     寄存器的级数
+ * @param width 每个寄存器的位宽
+ */
+class ShiftRegister(n: Int, width: Int) extends Module {
   //定义了此模块的输入与输出接口
   val io = IO(new Bundle {
-    val in = Input(Bool())
+    val in = Input(UInt(width.W))
+    val load = Input(UInt((n * width).W))
     val out = Output(UInt(width.W))
+    val control = Input(UInt(2.W))                //接收一个2位的控制信号：0保持、1左移、2右移、3并行加载
+    val dataOut = Output(Vec(n, UInt(width.W)))   //输出整个寄存器数组
   })
 
-  //定义了寄存器reg，类型为无符号整数，位宽为width
-  val reg = RegInit(0.U(width.W))
+  // 定义并初始化寄存器数组，初始值为0
+  val regs = RegInit(VecInit(Seq.fill(n)(0.U(width.W))))
+  val nextRegs = Wire(Vec(n, UInt(width.W)))       //计算每个寄存器下一时刻的值，默认保持当前值
+  for(i <- 0 until n) {
+    nextRegs(i) := regs(i)
+  }
 
-  //实现了右移寄存器，每次将原数据的低width-1位和输入进行拼接
-  reg := Cat(reg(width-2, 0), io.in)
-  io.out := reg
+  switch(io.control){
+    is(0.U){ }   //控制信号0: 保持现状
+    is(1.U){    //控制信号1: 左移
+      nextRegs(0) := io.in   //从输入中得到数据
+      for(i <- 1 until n){
+        nextRegs(i) := regs(i-1)
+      }
+    }
+    is(2.U){    //控制信号2: 右移
+      nextRegs(n-1) := io.in
+      for(i <- 0 until n-1){
+        nextRegs(i) := regs(i+1)
+      }
+    }
+    is(3.U) {   //控制信号3: 并行加载：将一个拼接后的大数据加载到各个寄存器中
+      for(i <- 0 until n) {
+        nextRegs(i) := io.load((i + 1) * width - 1, i * width) // 将输入 load 按 width 位进行切片
+      }
+    }
+  }
+
+  regs := nextRegs
+  io.out := regs(n - 1)
+  io.dataOut := regs
+
 }
--- src/test/scala/ShiftRegister/ShiftRegisterTest.scala
+++ src/test/scala/ShiftRegister/ShiftRegisterTest.scala
@@ -6,19 +6,55 @@ import org.scalatest.flatspec.AnyFlatSpec
 import chisel3.simulator.EphemeralSimulator._
 
 class ShiftRegisterTest extends AnyFlatSpec{
-  "ShiftRegister" should "shift correctly" in{
-    simulate(new ShiftRegister(4)){ dut =>
-      dut.io.in.poke(1)
+  "ShiftRegister" should "保持状态 when control = 0" in{
+    simulate(new ShiftRegister(4, 8)){ dut =>
+      dut.io.load.poke("h44332211".U)
+      dut.io.control.poke(3.U)
       dut.clock.step(1)
-      dut.io.out.expect("b0001".U)
 
-      dut.io.in.poke(0)
+      dut.io.in.poke("hAA".U)
+      dut.io.control.poke(0.U)
       dut.clock.step(1)
-      dut.io.out.expect("b0010".U)
 
-      dut.io.in.poke(1)
-      dut.clock.step(1)
-      dut.io.out.expect("b0101".U)
+      dut.io.out.expect("h44".U)
     }
+  }
+  "ShiftRegister" should "向左移位 when control = 1" in{
+    simulate(new ShiftRegister(4, 8)){ dut =>
+      dut.io.load.poke("hAABBCCDD".U)
+      dut.io.control.poke(3.U)
+      dut.clock.step(1)
+
+      dut.io.control.poke(1.U)
+      dut.io.in.poke("hAA".U)
+      dut.clock.step(1)
+      dut.io.control.poke(1.U)
+      dut.io.in.poke("hBB".U)
+      dut.clock.step(1)
+      dut.io.dataOut(0).expect("hBB".U)
+      dut.io.dataOut(1).expect("hAA".U)
+      dut.io.dataOut(2).expect("hDD".U)
+      dut.io.dataOut(3).expect("hCC".U)
+
+      dut.io.out.expect("hCC".U)
+    }
+  }
+  "ShiftRegister" should "向右移动 when control = 2" in {
+      simulate(new ShiftRegister(4, 8)) { dut =>
+        dut.io.load.poke("h44332211".U)
+        dut.io.control.poke(3.U)
+        dut.clock.step(1)
+
+
+        dut.io.control.poke(2.U)
+        dut.io.in.poke("hFF".U)
+        dut.clock.step(1)
+        dut.io.dataOut(0).expect("h22".U)
+        dut.io.dataOut(1).expect("h33".U)
+        dut.io.dataOut(2).expect("h44".U)
+        dut.io.dataOut(3).expect("hFF".U)
+
+        dut.io.out.expect("hFF".U)
+      }
   }
 }
