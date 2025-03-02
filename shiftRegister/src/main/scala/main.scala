dd03a097e9cf4fcf74a58d15f4d2ebd6d4a45572
d6dtd <2450028253@qq.com>
2025-03-02 18:50:27 +0800

    移位寄存器v3

--- shiftRegister/src/main/scala/main.scala
+++ shiftRegister/src/main/scala/main.scala
@@ -1,30 +1,29 @@
 
 import chisel3._
 
-//def cmpGreater(a: UInt(c.W), b: UInt(d.W)): bool = {
-//    //   比较最高位
-//    //   0 - 0 = 1 <
-//    //   1 - 0 >
-//    //   0 - 1 <
-//    //   1 - 1 = 1 >
-//    //
-//    val res = Wire()
-//    res := a - b
-//    return 1
-//}
+object Functions {
+    def cmpGreater(a: UInt, b: UInt): Bool = {
+        // TODO 比较器
+        return a > b
+    }
+}
 
-class SRBlock extends Module {
+class SRBlock(pWidth: Int, dataWidth : Int) extends Module {
+
     val io = IO(new Bundle{
         val read = Input(Bool())
         val write = Input(Bool())
-        val newEntryBus = Input(UInt(16.W))
-        val leftEntry = Input(UInt(16.W))
-        val rightEntry = Input(UInt(16.W))
 
-        val outputEntry = Output(UInt(16.W))
+        // 低位优先级，高位元数据
+        val newEntryBus = Input(UInt(pWidth.W + dataWidth.W))
+        val leftEntry = Input(UInt(pWidth.W + dataWidth.W))
+        val rightEntry = Input(UInt(pWidth.W + dataWidth.W))
+
+        val outputEntry = Output(UInt(pWidth.W + dataWidth.W))
     })
 
-    val entry = RegInit(~0.U(16.W)) // 初始为全1，优先级最低
+    val entry = RegInit(~0.U(pWidth.W + dataWidth.W)) // 初始为全1，优先级最低
+
     io.outputEntry := entry
 
     when(io.read && !io.write) {
@@ -35,41 +34,44 @@ class SRBlock extends Module {
         // 若自己的值比new entry大且右边的值比new entry大，则本entry向左移动
         // 若自己的值比new entry大且右边的值比new entry小，则本entry向左移动且new entry应该存在本block
         // 若自己的值比new entry小则位置不变
-        // TODO 比较要修改
-        when (entry > io.newEntryBus) {
-            entry := Mux(io.rightEntry > io.newEntryBus, io.rightEntry, io.newEntryBus)
+        when ( Functions.cmpGreater(entry(pWidth - 1, 0), io.newEntryBus(pWidth - 1, 0)) ) {
+            entry := Mux(Functions.cmpGreater(io.rightEntry(pWidth - 1, 0),
+                io.newEntryBus(pWidth - 1, 0)) , io.rightEntry, io.newEntryBus)
         }
     }.otherwise {
-
+        // 无操作
     }
 }
 
-class ShiftRegister extends Module {
+/*
+* pWidth:  优先级位宽
+* dataWidth: 元数据位宽
+* blockCount: block个数
+* */
+class ShiftRegister(pWidth: Int, dataWidth : Int, blockCount : Int) extends Module {
     val io = IO(new Bundle{
         val read = Input(UInt(1.W))
         val write = Input(UInt(1.W))
-        val newEntry = Input(UInt(16.W))
-        val highestEntry = Output(UInt(16.W))
+        val newEntry = Input(UInt(pWidth.W + dataWidth.W))
+        val highestEntry = Output(UInt(pWidth.W + dataWidth.W))
     })
 
-    val newEntryBus = Wire(UInt(16.W))
+    val newEntryBus = Wire(UInt(pWidth.W + dataWidth.W))
     newEntryBus := io.newEntry
 
-    val size = 10 // SR的规模
-
-    val blocks = Seq.fill(size)(Module(new SRBlock()))
+    val blocks = Seq.fill(blockCount)(Module(new SRBlock(pWidth, dataWidth)))
 
     // 最左边block的左输入为Invalid Entry
-    blocks(size - 1).io.leftEntry := ~0.U(16.W)
+    blocks(blockCount - 1).io.leftEntry := ~0.U(pWidth.W)
 
     // 最右边block的右输入为0，表示最大优先级
-    blocks(0).io.rightEntry := 0.U(16.W)
+    blocks(0).io.rightEntry := 0.U(pWidth.W)
 
     // 最右边block的输出则为整个PQ的输出
     io.highestEntry := blocks(0).io.outputEntry
 
     // 连接其他线
-    for (i <- 0 until size) {
+    for (i <- 0 until blockCount) {
         // 所有block都要连接这三个信号
         blocks(i).io.read := io.read
         blocks(i).io.write := io.write
@@ -79,7 +81,7 @@ class ShiftRegister extends Module {
         if (i != 0) {
             blocks(i).io.rightEntry := blocks(i - 1).io.outputEntry
         }
-        if (i != size - 1) {
+        if (i != blockCount - 1) {
             blocks(i).io.leftEntry := blocks(i + 1).io.outputEntry
         }
     }
@@ -87,7 +89,5 @@ class ShiftRegister extends Module {
 
 object Main extends App {
     println("Hello Chisel World")
-    emitVerilog(new ShiftRegister())
-    val s = getVerilogString(new ShiftRegister())
-    println(s)
+    emitVerilog(new ShiftRegister(16, 16, 10))
 }
--- shiftRegister/src/test/scala/test.scala
+++ shiftRegister/src/test/scala/test.scala
@@ -4,17 +4,35 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class Test extends  AnyFlatSpec with ChiselScalatestTester {
     "ShiftRegister" should "correct" in {
-        test(new ShiftRegister)
+        test(new ShiftRegister(16, 0, 10))
             .withAnnotations(Seq(WriteVcdAnnotation))  { dut =>
+
+                // 插入10
+            dut.io.read.poke(0.U)
             dut.io.write.poke(1.U)
             dut.io.newEntry.poke(10.U)
             dut.clock.step()
             dut.io.highestEntry.expect(10.U)
-            dut.io.write.poke(0.U)
-            dut.io.read.poke(1.U)
-            dut.clock.step()
-            dut.io.highestEntry.expect(65535.U)
 
+                // 插入30
+            dut.io.read.poke(0.U)
+            dut.io.write.poke(1.U)
+            dut.io.newEntry.poke(30.U)
+            dut.clock.step()
+            dut.io.highestEntry.expect(10.U)
+
+                // 弹出10
+            dut.io.read.poke(1.U)
+            dut.io.write.poke(0.U)
+            dut.clock.step()
+            dut.io.highestEntry.expect(30.U)
+
+                // 插入20
+            dut.io.read.poke(0.U)
+            dut.io.write.poke(1.U)
+            dut.io.newEntry.poke(20.U)
+            dut.clock.step()
+            dut.io.highestEntry.expect(20.U)
         }
     }
 }
