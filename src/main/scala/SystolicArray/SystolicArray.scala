19da1f4f33d629775806d72f128ca810b9f77700
Burry <mfsang@EthanSang-2.local>
2025-03-04 21:09:37 +0800

    初始化了SystolicArray项目

--- src/main/scala/SystolicArray/SystolicArray.scala
+++ src/main/scala/SystolicArray/SystolicArray.scala
@@ -3,6 +3,20 @@ package SystolicArray
 import chisel3._
 import chisel3.util._
 
+class PriorityEntry(val priorityWidth: Int, val flowWidth: Int) extends Bundle {
+  val priority = UInt(priorityWidth.W)      // 定义元素的优先级
+  val flowId   = UInt(flowWidth.W)          // 定义元素的流标号
+}
+
+object PriorityEntry {
+  def default(priorityWidth: Int, flowWidth: Int): PriorityEntry = {
+    val flow_element = Wire(new PriorityEntry(priorityWidth, flowWidth))
+    flow_element.priority := 0.U            // 对元素进行初始化
+    flow_element.flowId   := 0.U
+    flow_element
+  }
+}
+
 class SystolicArray {
 
 }
