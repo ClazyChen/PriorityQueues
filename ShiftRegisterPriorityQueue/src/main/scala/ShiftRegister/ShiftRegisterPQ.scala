871f11418d94189a2bfdc302b8a29e40a3b686a6
leviAck <2481538544@qq.com>
2025-03-08 17:17:32 +0800

    rebuild v2

--- ShiftRegisterPriorityQueue/src/main/scala/ShiftRegister/ShiftRegisterPQ.scala
+++ ShiftRegisterPriorityQueue/src/main/scala/ShiftRegister/ShiftRegisterPQ.scala
@@ -5,35 +5,35 @@ import chisel3.util._;
 
 
 // 定义entry结构体
-class EntryBlock (dataWidth : Int,priorityWidth : Int) extends Bundle {
+class EntryBlock (dataWidth : Int,rankWidth : Int) extends Bundle {
     val data = UInt(dataWidth.W);                                             
-    val priority = UInt(priorityWidth.W);                                           
+    val rank = UInt(rankWidth.W);                                           
 }
 
 // 定义EntryBlock伴生对象，用工厂方法初始化默认entry
 object EntryBlock {
-    def default(dataWidth : Int,priorityWidth : Int) : EntryBlock = {
-        val entry = Wire(new EntryBlock(dataWidth,priorityWidth));
+    def default(dataWidth : Int,rankWidth : Int) : EntryBlock = {
+        val entry = Wire(new EntryBlock(dataWidth,rankWidth));
         entry.data := 0.U(dataWidth.W);                                       
-        entry.priority := (-1.S(priorityWidth.W)).asUInt;                              
-        entry
+        entry.rank := (-1.S(rankWidth.W)).asUInt;                              
+        entry 
     }
 }
 
 // 定义ShiftRegisterBlock
 // prev:下标较小一侧，nxt：下标较大一侧
-class ShiftRegisterBlock(dataWidth : Int,priorityWidth : Int) extends Module{
+class ShiftRegisterBlock(dataWidth : Int,rankWidth : Int) extends Module{
 
     // 内部io接口
     val io = IO(new Bundle{
-        val input_prev = Input(new EntryBlock(dataWidth,priorityWidth));
-        val input_nxt = Input(new EntryBlock(dataWidth,priorityWidth));
-        val output_prev = Output(new EntryBlock(dataWidth,priorityWidth));
-        val output_nxt = Output(new EntryBlock(dataWidth,priorityWidth));
+        val input_prev = Input(new EntryBlock(dataWidth,rankWidth));
+        val input_nxt = Input(new EntryBlock(dataWidth,rankWidth));
+        val output_prev = Output(new EntryBlock(dataWidth,rankWidth));
+        val output_nxt = Output(new EntryBlock(dataWidth,rankWidth));
         val insert_cur_index = Input(Bool());
-        val cur_input_entry = Input(new EntryBlock(dataWidth,priorityWidth));
-        val cur_output_entry = Output(new EntryBlock(dataWidth,priorityWidth));
-        val cmd = Input(Valid(UInt(1.W)));
+        val cur_input_entry = Input(new EntryBlock(dataWidth,rankWidth));
+        val cur_output_entry = Output(new EntryBlock(dataWidth,rankWidth));
+        val cmd = Input(Valid(UInt(2.W)));
     });
     
     // 每个shiftregister内部的控制信号
@@ -41,7 +41,7 @@ class ShiftRegisterBlock(dataWidth : Int,priorityWidth : Int) extends Module{
     val ENQ = 1.U(2.W);
 
     // 用来保存entry的寄存器
-    val entry_holder = RegInit(EntryBlock.default(dataWidth,priorityWidth));
+    val entry_holder = RegInit(EntryBlock.default(dataWidth,rankWidth));
 
     // initialization output
     io.output_prev := entry_holder;
@@ -55,18 +55,15 @@ class ShiftRegisterBlock(dataWidth : Int,priorityWidth : Int) extends Module{
                 entry_holder := io.input_nxt; // 从下标大的一侧输入 右移
             }
             is (ENQ) {  
-                when (insert_cur_index) {
+                when (io.insert_cur_index) {
                     entry_holder := io.cur_input_entry; // latch
                 }           
-                .elsewhen(entry_holder.priority < io.cur_input_entry.priority) {
+                .elsewhen(entry_holder.rank > io.cur_input_entry.rank) { // to-be-fixed
                     entry_holder := io.input_prev; // 从下标小的一侧输入 左移
                 }
                 .otherwise {
                     // do nothing
                 }
-            }
-            otherwise {
-                // do nothing
             }
         }
     }
@@ -76,7 +73,7 @@ class ShiftRegisterBlock(dataWidth : Int,priorityWidth : Int) extends Module{
 }
 
 // 定义top-module，优先队列结构
-class PriorityQueueBlock(dataWidth: Int,priorityWidth : Int,depth : Int) extends Module {
+class PriorityQueueBlock(dataWidth: Int,rankWidth : Int,depth : Int) extends Module {
 
     // 参数校验
     require(depth > 0, "Depth must be greater than 0");
@@ -85,8 +82,8 @@ class PriorityQueueBlock(dataWidth: Int,priorityWidth : Int,depth : Int) extends
     val io = IO(new Bundle{
         val read = Input(Bool());
         val write = Input(Bool());
-        val new_entry = Input(new EntryBlock(dataWidth,priorityWidth)); 
-        val output_entry = Output(new EntryBlock(dataWidth,priorityWidth));
+        val new_entry = Input(new EntryBlock(dataWidth,rankWidth)); 
+        val output_entry = Output(new EntryBlock(dataWidth,rankWidth));
     });
 
     // 对应的控制信号
@@ -94,10 +91,10 @@ class PriorityQueueBlock(dataWidth: Int,priorityWidth : Int,depth : Int) extends
     val ENQ = 1.U(2.W);
     val PULSE = 2.U(2.W);
     val cmd_valid = true.B;
-    val cmd = Mux(io.wirte,ENQ,Mux(io.read,DEQ,PULSE));
+    val cmd = Mux(io.write,ENQ,Mux(io.read,DEQ,PULSE));
 
     // 生成depth个ShiftRegisterBlock
-    val blocks = Seq.fill(depth)(Module(new ShiftRegisterBlock(dataWidth,priorityWidth)));
+    val blocks = Seq.fill(depth)(Module(new ShiftRegisterBlock(dataWidth,rankWidth)));
 
     // 模块连接
     for (i <- 0 until (depth - 1)) {
@@ -111,16 +108,20 @@ class PriorityQueueBlock(dataWidth: Int,priorityWidth : Int,depth : Int) extends
         blocks(i).io.cur_input_entry := io.new_entry;
     }
 
-    blocks(depth - 1).io.input_nxt := EntryBlock.default(dataWidth,priorityWidth);
+    blocks(depth - 1).io.input_nxt := EntryBlock.default(dataWidth,rankWidth);
+    blocks(0).io.input_prev := EntryBlock.default(dataWidth,rankWidth);
+
     blocks(0).io.output_prev <> io.output_entry;
 
     // 计算插入位置
-    val is_less := WireInit(VecInit(Seq.fill(depth)(false.B)));
+    val is_less = WireInit(VecInit(Seq.fill(depth)(false.B)));
     for (i <- 0 until depth) {
-        is_less(i) := (blocks(i).cur_output_entry.priority < io.new_entry.priority); 
+        is_less(i) := (blocks(i).io.cur_output_entry.rank > io.new_entry.rank); 
     }
-    val insert_index = Wire(UInt(depth.W));
-    insert_index := Cat(is_less.reverse);
+    val reverse_is_less = Wire(UInt(depth.W));
+    reverse_is_less := Cat(is_less.reverse);
+
+    val insert_index = PriorityEncoder(reverse_is_less);
 
     // 为每个ShiftRegisterBlock生成标志位
     for (i <- 0 until depth) {
@@ -130,10 +131,9 @@ class PriorityQueueBlock(dataWidth: Int,priorityWidth : Int,depth : Int) extends
         .otherwise {
             blocks(i).io.insert_cur_index := false.B;
         }
-    }
-
-    
+    }  
 }
+
 
 // 入口
 object Main extends App {
--- ShiftRegisterPriorityQueue/src/test/scala/ShiftRegister/ShiftRegisterTest.scala
+++ ShiftRegisterPriorityQueue/src/test/scala/ShiftRegister/ShiftRegisterTest.scala
@@ -2,37 +2,58 @@
 // https://scalameta.org/munit/docs/getting-started.html
 package ShiftRegisterPQ
 
-import chisel3.util._;
 import chisel3._;
+import chisel3.util._;
+import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec;
-import chisel3.simulator.EphemeralSimulator._
 
+class ShiftRegisterPriorityQueueTest extends AnyFlatSpec with ChiselScalatestTester {
 
-class ShiftRegisterPriorityQueueTest extends AnyFlatSpec {
+    def dequeue (c : PriorityQueueBlock) : UInt = {
+        c.io.read.poke(true.B);
+        c.io.write.poke(false.B);
+        c.clock.step(1);
+        ()
+    }
+
+    def enqueue (c : PriorityQueueBlock,rank : Int) : UInt = {
+        c.io.write.poke(true.B);
+        c.io.read.poke(false.B);
+        c.io.new_entry.rank.poke(rank.U);
+        c.clock.step(1);
+        ()
+    }
+
     "PriorityQueueBlock" should "enqueue and dequeue entries properly" in {
-        simulate(new PriorityQueueBlock(0,4,4)) { c => 
+
+        test(new PriorityQueueBlock(0,4,4)) { c => 
+
+        c.io.output_entry.expect(0xf.U);
 
         // 入队操作
         // 初始化信号
-        c.io.read.poke(false.B)
-        c.io.write.poke(true.B)
-        c.io.new_entry.valid.poke(true.B)
-        c.io.new_entry.bits.address.poke(1.U)
-        c.io.new_entry.bits.priority.poke(5.U)
+        // c.io.read.poke(false.B)
+        // c.io.write.poke(true.B)
+        // c.io.new_entry.valid.poke(true.B)
+        // c.io.new_entry.bits.address.poke(1.U)
+        // c.io.new_entry.bits.priority.poke(5.U)
 
-        // 推进时钟
-        c.clock.step(1)
+        // // 推进时钟
+        // c.clock.step(1)
 
-        // 检查队列状态
-        c.io.new_entry.valid.poke(false.B)
-        c.io.write.poke(false.B)
-        c.clock.step(1)
+        // // 检查队列状态
+        // c.io.new_entry.valid.poke(false.B)
+        // c.io.write.poke(false.B)
+        // c.clock.step(1)
 
-        // 查看队首条目
-        c.io.read.poke(true.B)
-        c.clock.step(1)
-        c.io.output_entry.address.expect(1.U)
-        c.io.output_entry.priority.expect(5.U)
+        // // 查看队首条目
+        // c.io.read.poke(true.B)
+        // c.clock.step(1)
+        // c.io.output_entry.address.expect(1.U)
+        // c.io.output_entry.priority.expect(5.U)
+
+        // 初始态
+        
 
 
         }
