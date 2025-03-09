056bc23f3e1aaa2aa56c54f3bae5deac192e1a86
d6dtd <2450028253@qq.com>
2025-03-09 17:52:23 +0800

    模块化

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,5 +3,4 @@ import chisel3._
 object Main extends App {
     println("Hello Chisel World")
     emitVerilog(new ShiftRegister(0, 16, 10))
-    emitVerilog(new Systolic(0, 16, 10))
 }
--- src/main/scala/shiftRegister/shiftRegister.scala
+++ src/main/scala/shiftRegister/shiftRegister.scala
@@ -1,43 +1,60 @@
 
 import chisel3._
 
+class ShiftRegisterBlock(dataWidth: Int, rWidth: Int) extends Module{
+    val io = IO(new Bundle {
+        val enqueue = Input(Bool())
+        val dequeue = Input(Bool())
+        val highEntry = Input(new Entry(dataWidth, rWidth))   // 上级的输入
+        val lowEntry = Input(new Entry(dataWidth, rWidth))    // 下级的输入
+        val highCmp = Input(Bool())                           // 上级的比较结果
+        val newEntry = Input(new Entry(dataWidth, rWidth))    // 下级的输入
+        val outputEntry = Output(new Entry(dataWidth, rWidth))
+        val outputCmp = Output(Bool())
+    })
+
+    val entry = RegInit(Entry.default(dataWidth, rWidth))
+    val cmp = WireInit(false.B) // 比较结果
+
+    io.outputEntry := entry
+    io.outputCmp := false.B
+
+    when(io.dequeue && !io.enqueue) {
+        entry := io.lowEntry
+    }.elsewhen(!io.dequeue && io.enqueue) {
+        cmp := entry.rank > io.newEntry.rank
+        io.outputCmp := cmp
+
+        when(cmp) {
+            entry := Mux(io.highCmp, io.highEntry, io.newEntry)
+        }
+    }
+}
+
 class ShiftRegister(dataWidth: Int, rWidth: Int, capacity : Int) extends Module {
     val io = IO(new Bundle{
-        val dequeue = Input(Bool())
         val enqueue = Input(Bool())
+        val dequeue = Input(Bool())
         val newEntry = Input(new Entry(dataWidth, rWidth))
         val highestEntry = Output(new Entry(dataWidth, rWidth))
     })
 
-    val entryArray = RegInit(VecInit(Seq.fill(capacity)(Entry.default(dataWidth, rWidth) )))
-    val cmpArray = WireInit(VecInit(Seq.fill(capacity)(false.B))) // 暂存比较结果
+    val blockArray = Seq.fill(capacity)(Module(new ShiftRegisterBlock(dataWidth, rWidth)))
 
-    when(io.dequeue && !io.enqueue) {
-        // dequeue时向右移动
-        for (i <- 0 until capacity - 1) {
-            entryArray(i) := entryArray(i + 1)
-        }
-        entryArray(capacity - 1) := Entry.default(dataWidth, rWidth)
-    }.elsewhen(!io.dequeue && io.enqueue) {
-        // 先暂存比较结果
-        for (i <- 0 until capacity) {
-            cmpArray(i) := entryArray(i).rank > io.newEntry.rank
-        }
+    for (i <- 0 until capacity) {
+        blockArray(i).io.enqueue := io.enqueue
+        blockArray(i).io.dequeue := io.dequeue
+        blockArray(i).io.newEntry := io.newEntry
 
-        // 对0号元素单独处理
-        when(cmpArray(0)) {
-            entryArray(0) := io.newEntry
+        if (i > 0) {
+            blockArray(i).io.highEntry := blockArray(i - 1).io.outputEntry
+            blockArray(i).io.highCmp := blockArray(i - 1).io.outputCmp
         }
-
-        // 对其余元素处理
-        for (i <- 1 until capacity) {
-            when (cmpArray(i)) {
-                entryArray(i) := Mux(cmpArray(i - 1), entryArray(i - 1), io.newEntry)
-            }
-        }
-    }.otherwise {
-        // 无操作
+        if (i < capacity - 1) blockArray(i).io.lowEntry := blockArray(i + 1).io.outputEntry
     }
+    blockArray(0).io.highEntry := Entry.default(dataWidth, rWidth) // 这个输入不会产生作用
+    blockArray(0).io.highCmp := false.B
+    blockArray(capacity - 1).io.lowEntry := Entry.default(dataWidth, rWidth)
 
-    io.highestEntry := entryArray(0)
+    io.highestEntry := blockArray(0).io.outputEntry
 }
