83ec25a2db259d5814d4aa5e5044195b7721ad07
d6dtd <2450028253@qq.com>
2025-03-09 18:15:28 +0800

    test添加函数

--- src/test/scala/SRTest.scala
+++ src/test/scala/SRTest.scala
@@ -5,44 +5,45 @@ import scala.util.Random
 import scala.collection.mutable.PriorityQueue
 
 class SRTest extends  AnyFlatSpec with ChiselScalatestTester {
+    def enqueue(dut: ShiftRegister, rank: Int, expectRank: Int): Unit = {
+        dut.io.dequeue.poke(false.B)
+        dut.io.enqueue.poke(true.B)
+        dut.io.newEntry.rank.poke(rank.U)
+        dut.io.newEntry.metadata.poke(0)
+        dut.clock.step()
+        dut.io.highestEntry.rank.expect(expectRank.U)
+    }
+
+    def dequeue(dut: ShiftRegister, expectRank: Int): Unit = {
+        dut.io.highestEntry.rank.expect(expectRank.U)
+        dut.io.dequeue.poke(true.B)
+        dut.io.enqueue.poke(false.B)
+        dut.clock.step()
+    }
+
     "ShiftRegister" should "correct" in {
         test(new ShiftRegister(0, 16, 20))
             .withAnnotations(Seq(WriteVcdAnnotation)) {dut =>
                 val randomArray = Array.fill(50)(Random.nextInt(65535))
                 val sortedArray = randomArray.sorted
                 var minValue = 65535
-
                 // 重复两次
                 for (_ <- 0 until 2) {
                     minValue = 65535
                     for (i <- 0 until 50) {
-                        dut.io.dequeue.poke(false.B)
-                        dut.io.enqueue.poke(true.B)
-                        dut.io.newEntry.rank.poke(randomArray(i))
-                        dut.io.newEntry.metadata.poke(0)
-                        dut.clock.step()
                         if (minValue > randomArray(i)) {
                             minValue = randomArray(i)
                         }
-                        dut.io.highestEntry.rank.expect(minValue.U)
+                        enqueue(dut, randomArray(i), minValue)
                     }
-
                     // 验证队列中是否剩下20个优先级最高元素
-                    for (i <- 0 until 19) {
-                        dut.io.dequeue.poke(true.B)
-                        dut.io.enqueue.poke(false.B)
-                        dut.clock.step()
-                        dut.io.highestEntry.rank.expect(sortedArray(i + 1).U)
+                    for (i <- 0 until 20) {
+                        dequeue(dut, sortedArray(i))
                     }
-
                     // 剩下应该都是空
                     for (i <- 0 until 20) {
-                        dut.io.dequeue.poke(true.B)
-                        dut.io.enqueue.poke(false.B)
-                        dut.clock.step()
-                        dut.io.highestEntry.rank.expect(65535.U)
+                        dequeue(dut, 65535)
                     }
-
                 }
             }
         test(new ShiftRegister(0, 16, 50))
@@ -54,21 +55,14 @@ class SRTest extends  AnyFlatSpec with ChiselScalatestTester {
                         if (minPriorityQueue.size < 50) {
                             val rank = Random.nextInt(65535)
                             minPriorityQueue.enqueue(rank)
-                            dut.io.dequeue.poke(false.B)
-                            dut.io.enqueue.poke(true.B)
-                            dut.io.newEntry.rank.poke(rank.U)
-                            dut.io.newEntry.metadata.poke(0)
-                            dut.clock.step()
+                            enqueue(dut, rank, minPriorityQueue.head)
                         }
                     } else {
                         if (minPriorityQueue.nonEmpty) {
-                            dut.io.highestEntry.rank.expect(minPriorityQueue.dequeue().U)
+                            dequeue(dut, minPriorityQueue.dequeue())
                         } else {
-                            dut.io.highestEntry.rank.expect(65535.U)
+                            dequeue(dut, 65535)
                         }
-                        dut.io.dequeue.poke(true.B)
-                        dut.io.enqueue.poke(false.B)
-                        dut.clock.step()
                     }
                 }
             }
