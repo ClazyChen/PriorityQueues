eb0e459ed8f89d7d2e841a150bf54a0fbd7e4244
leviAck <2481538544@qq.com>
2025-03-09 21:13:31 +0800

    update SystolicArray v2

--- SystolicArrayPriorityQueue/src/main/scala/SystolicArray/SystolicArrayPQ.scala
+++ SystolicArrayPriorityQueue/src/main/scala/SystolicArray/SystolicArrayPQ.scala
@@ -28,37 +28,48 @@ class SystolicArrayBlock(dataWidth : Int,rankWidth : Int) extends Module {
         val output_prev = Output(new EntryBlock(dataWidth,rankWidth));      // 向前一个block输出的entry
         val output_nxt = Output(new EntryBlock(dataWidth,rankWidth));       // 向后一个block输出的entry
         val cur_output_entry = Output(new EntryBlock(dataWidth,rankWidth)); // 当前block保存的entry，用一个接口供外部访问
-        val cmd = Input(Valid(UInt(2.W)));                                  // cmd 表示当前block接受的指令
+        val cmd_in = Input(Valid(UInt(2.W)));                               // 当前block接受的控制信号
+        val cmd_out = Output(Valid(UInt(2.W)));                             // cmd//表示向下一个block传递的指令
     });
 
     // 声明控制信号
     val DEQ = 0.U(2.W);
     val ENQ = 1.U(2.W);
+    val PULSE = 2.U(2.W);
 
     // 内部的两个寄存器
     val entry_holder = RegInit(EntryBlock.default(dataWidth,rankWidth));    // 保存当前entry的寄存器
-    val low_priority_holder = RegInit(DataBlock.default(dataWidth,rankWidth));       // 记录当前优先级较小entry的寄存器，这个寄存器内的内容用于向下标大的块传递
+    val low_priority_holder = RegInit(EntryBlock.default(dataWidth,rankWidth));       // 记录当前优先级较小entry的寄存器，这个寄存器内的内容用于向下标大的块传递
 
     // initialization output
     io.output_prev := entry_holder;
     io.output_nxt := low_priority_holder;
-    io.cur_output_entry := entry_holder
+    io.cur_output_entry := entry_holder;
+    io.cmd_out.valid := true.B;
+    io.cmd_out.bits := PULSE;
 
     // Systolic Array decision block
-     when (io.cmd.valid) {
-        switch (io.cmd.bits) {
+     when (io.cmd_in.valid){
+        switch (io.cmd_in.bits) {
             is(DEQ) {
                 entry_holder := io.input_nxt; 
             }
-            is (ENQ) {  
-               when (low_priority_holder.rank  > io.input_prev.rank) {
-                 entry_holder := io.input_prev;
-                 tmp_holder := io.input_prev;
+            is (ENQ) {          // 新entry入队信号  to be completed
+               when (entry_holder.rank <= io.input_prev.rank) {
+                    low_priority_holder := io.input_prev; 
+
+               }
+               .elsewhen (entry_holder.rank > io.input_prev.rank) {
+                    low_priority_holder := entry_holder; 
+                    entry_holder := io.input_prev; 
+               }
+               .otherwise {
+                    // do nothing
                }
             }
         }
     }
-    .otherwise{
+    .otherwise {
         // do nothing
     }
 }
@@ -82,7 +93,7 @@ class PriorityQueueBlock(dataWidth: Int,rankWidth : Int,depth : Int) extends Mod
     val ENQ = 1.U(2.W);
     val PULSE = 2.U(2.W); // 表示系统暂停，在这里没有使用到
     val cmd_valid = true.B; // 默认新到达的entry都是有效的
-    val cmd = Mux(io.write,ENQ,Mux(io.read,DEQ,PULSE)); // 产生控制信号
+    val cmd_status = Mux(io.write,ENQ,Mux(io.read,DEQ,PULSE)); // 产生控制信号
 
     // 生成depth个SystolicArrayBlock
     val blocks = Seq.fill(depth)(Module(new SystolicArrayBlock(dataWidth,rankWidth)));
@@ -91,20 +102,24 @@ class PriorityQueueBlock(dataWidth: Int,rankWidth : Int,depth : Int) extends Mod
     for (i <- 0 until (depth - 1)) {
         blocks(i + 1).io.input_prev <> blocks(i).io.output_nxt;
         blocks(i).io.input_nxt <> blocks(i + 1).io.output_prev;
+        blocks(i + 1).io.cmd_in.valid := blocks(i).io.cmd_out.valid;
+        blocks(i + 1).io.cmd_in.bits := blocks(i).io.cmd_out.bits
     }
 
     // 给未初始化的sink初始化
     blocks(depth - 1).io.input_nxt := EntryBlock.default(dataWidth,rankWidth);
-    blocks(0).io.input_prev := EntryBlock.default(dataWidth,rankWidth);
 
-    // 连接0号块与外部接口
-    blocks(0).
+    // 0号块前一个块的输入是系统新到达的entry
+    blocks(0).io.input_prev <> io.new_entry;
 
     // 最终的输出等于0号block向前的输出
     blocks(0).io.output_prev <> io.output_entry;
 
-    // 计算插入位置
-    
+    // 0号控制信号初始化
+    blocks(0).io.cmd_in.valid := cmd_valid;
+    blocks(0).io.cmd_in.bits := cmd_status;
+
+    blocks(0).io.cmd_out <> blocks(0).io.cmd_in;
 
 }
 
--- SystolicArrayPriorityQueue/src/test/scala/SystolicArray/SystolicArrayTest.scala
+++ SystolicArrayPriorityQueue/src/test/scala/SystolicArray/SystolicArrayTest.scala
@@ -11,6 +11,27 @@ class SystolicArrayTest extends AnyFlatSpec with ChiselScalatestTester {
 
         test(new PriorityQueueBlock(0,4,4)) { c =>
             
+            // 初始态检验
+            c.clock.step(1);
+            c.io.read.poke(true.B);
+            c.io.write.poke(false.B);
+            c.io.output_entry.rank.expect(15);
+
+            c.io.read.poke(false.B);
+            c.io.write.poke(false.B);
+
+            // dequeue操作
+            c.io.read.poke(true.B);
+            c.io.write.poke(true.B);
+            c.io.output_entry.rank.expect(0xf.U);
+
+            // enqueue操作
+            c.io.read.poke(false.B);
+            c.io.write.poke(true.B);
+            c.io.new_entry.rank.poke(8.U);
+            c.clock.step(1);
+            c.io.output_entry.rank.expect(8.U);
+
 
         }
     }
