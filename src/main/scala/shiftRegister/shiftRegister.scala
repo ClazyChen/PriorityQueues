cb56da9c8ebe4eac7c82ee9cb361c20e245ba992
d6dtd <2450028253@qq.com>
2025-03-12 00:17:55 +0800

    开会修改内容

--- src/main/scala/shiftRegister/shiftRegister.scala
+++ src/main/scala/shiftRegister/shiftRegister.scala
@@ -1,45 +1,47 @@
 
 import chisel3._
 
-class ShiftRegisterBlock(dataWidth: Int, rWidth: Int) extends Module{
+class ShiftRegisterBlock(val dataWidth: Int, val rankWidth: Int) extends Module{
     val io = IO(new Bundle {
         val enqueue = Input(Bool())
         val dequeue = Input(Bool())
-        val highEntry = Input(new Entry(dataWidth, rWidth))   // 上级的输入
-        val lowEntry = Input(new Entry(dataWidth, rWidth))    // 下级的输入
-        val highCmp = Input(Bool())                           // 上级的比较结果
-        val newEntry = Input(new Entry(dataWidth, rWidth))    // 下级的输入
-        val outputEntry = Output(new Entry(dataWidth, rWidth))
+        val highEntry = Input(new Entry(dataWidth, rankWidth))   // 上级的输入
+        val lowEntry = Input(new Entry(dataWidth, rankWidth))    // 下级的输入
+        val highCmp = Input(Bool())                              // 上级的比较结果
+        val lowCmp = Input(Bool())                               // 下级的比较结果
+        val newEntry = Input(new Entry(dataWidth, rankWidth))
+        val outputEntry = Output(new Entry(dataWidth, rankWidth))
         val outputCmp = Output(Bool())
     })
 
-    val entry = RegInit(Entry.default(dataWidth, rWidth))
-    val cmp = WireInit(false.B) // 比较结果
-
+    val entry = RegInit(Entry.default(dataWidth, rankWidth))
+    val cmp = Wire(Bool()) // 比较结果
+    cmp := entry.rank > io.newEntry.rank
+    io.outputCmp := cmp
     io.outputEntry := entry
-    io.outputCmp := false.B
 
     when(io.dequeue && !io.enqueue) {
         entry := io.lowEntry
     }.elsewhen(!io.dequeue && io.enqueue) {
-        cmp := entry.rank > io.newEntry.rank
-        io.outputCmp := cmp
-
-        when(cmp) {
+        when (cmp) {
             entry := Mux(io.highCmp, io.highEntry, io.newEntry)
+        }
+    }.elsewhen(io.dequeue && io.enqueue) {
+        when (!cmp) {
+            entry := Mux(io.lowCmp, io.newEntry, io.lowEntry)
         }
     }
 }
 
-class ShiftRegister(dataWidth: Int, rWidth: Int, capacity : Int) extends Module {
+class ShiftRegister(val dataWidth: Int, val rankWidth: Int, val capacity : Int) extends Module {
     val io = IO(new Bundle{
         val enqueue = Input(Bool())
         val dequeue = Input(Bool())
-        val newEntry = Input(new Entry(dataWidth, rWidth))
-        val highestEntry = Output(new Entry(dataWidth, rWidth))
+        val newEntry = Input(new Entry(dataWidth, rankWidth))
+        val highestEntry = Output(new Entry(dataWidth, rankWidth))
     })
 
-    val blockArray = Seq.fill(capacity)(Module(new ShiftRegisterBlock(dataWidth, rWidth)))
+    val blockArray = Seq.fill(capacity)(Module(new ShiftRegisterBlock(dataWidth, rankWidth)))
 
     for (i <- 0 until capacity) {
         blockArray(i).io.enqueue := io.enqueue
@@ -50,11 +52,15 @@ class ShiftRegister(dataWidth: Int, rWidth: Int, capacity : Int) extends Module 
             blockArray(i).io.highEntry := blockArray(i - 1).io.outputEntry
             blockArray(i).io.highCmp := blockArray(i - 1).io.outputCmp
         }
-        if (i < capacity - 1) blockArray(i).io.lowEntry := blockArray(i + 1).io.outputEntry
+        if (i < capacity - 1) {
+            blockArray(i).io.lowEntry := blockArray(i + 1).io.outputEntry
+            blockArray(i).io.lowCmp := blockArray(i + 1).io.outputCmp
+        }
     }
-    blockArray(0).io.highEntry := Entry.default(dataWidth, rWidth) // 这个输入不会产生作用
+    blockArray(0).io.highEntry := DontCare
     blockArray(0).io.highCmp := false.B
-    blockArray(capacity - 1).io.lowEntry := Entry.default(dataWidth, rWidth)
+    blockArray(capacity - 1).io.lowEntry := Entry.default(dataWidth, rankWidth)
+    blockArray(capacity - 1).io.lowCmp := true.B
 
     io.highestEntry := blockArray(0).io.outputEntry
 }
--- src/main/scala/types.scala
+++ src/main/scala/types.scala
@@ -1,15 +1,15 @@
 import chisel3._
 
-class Entry(dataWidth: Int, rWidth: Int) extends Bundle {
+class Entry(dataWidth: Int, rankWidth: Int) extends Bundle {
     val metadata = UInt(dataWidth.W)
-    val rank = UInt(rWidth.W)
+    val rank = UInt(rankWidth.W)
 }
 
 object Entry {
-    def default(dataWidth: Int, rWidth: Int): Entry = {
-        val entry = Wire(new Entry(dataWidth, rWidth))
-        entry.metadata := 0.U(dataWidth.W)
-        entry.rank := -1.S(rWidth.W).asUInt
+    def default(dataWidth: Int, rankWidth: Int): Entry = {
+        val entry = Wire(new Entry(dataWidth, rankWidth))
+        entry.metadata := 0.U(dataWidth.W) // DontCare
+        entry.rank := -1.S(rankWidth.W).asUInt
         entry
     }
 }
