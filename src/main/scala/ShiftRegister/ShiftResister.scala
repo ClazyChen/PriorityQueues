414271c5a7d943563d5b839c873f3fcf3ca346b8
Yarasite <2573048690@qq.com>
2025-03-23 13:10:46 +0800

    只优化了代码,减少了比较器的使用并实现了replace,项目结构后续再修改

--- src/main/scala/ShiftRegister/ShiftResister.scala
+++ src/main/scala/ShiftRegister/ShiftResister.scala
@@ -1,78 +1,102 @@
 import chisel3._
 
-class Entry(dataWidth: Int, rankWidth: Int) extends Bundle {
-  val data = UInt(dataWidth.W)
-  val rank = UInt(rankWidth.W)
+class Entry(val dataWidth: Int, val rankWidth: Int) extends Bundle {
+    val data = UInt(dataWidth.W)
+    val rank = UInt(rankWidth.W)
 }
 
 object Entry {
-  def default(dataWidth: Int, rankWidth: Int): Entry = {
-    val e = Wire(new Entry(dataWidth, rankWidth))
-    // 默认rank为最低优先级
-    e.rank := -1.S(rankWidth.W).asUInt
-    e.data := 0.U(dataWidth.W)
-    e
-  }
+    def default(dataWidth: Int, rankWidth: Int): Entry = {
+        val e = Wire(new Entry(dataWidth, rankWidth))
+        // 默认rank为最低优先级
+        e.rank := -1.S(rankWidth.W).asUInt
+        e.data := 0.U(dataWidth.W)
+        e
+    }
 }
 
-// 每个block存储一个entry,通过left和right与相邻block通信
-class Block(dataWidth: Int, rankWidth: Int) extends Module {
-  val io = IO(new Bundle {
-    val read = Input(Bool())
-    val write = Input(Bool())
-    val new_entry = Input(new Entry(dataWidth, rankWidth))
-    val left = Input(new Entry(dataWidth, rankWidth))
-    val right = Input(new Entry(dataWidth, rankWidth))
-    val output = Output(new Entry(dataWidth, rankWidth))
-  })
+// 每个block存储一个entry,通过next和prev与相邻block通信
+class Block(val dataWidth: Int, val rankWidth: Int) extends Module {
+    val io = IO(new Bundle {
+        val read_in = Input(Bool())
+        val write_in = Input(Bool())
+        val cmp_prev_in = Input(Bool()) // new_entry_in < prev 为1
+        val cmp_next_in = Input(Bool())
+        val new_entry_in = Input(new Entry(dataWidth, rankWidth))
+        val next_in = Input(new Entry(dataWidth, rankWidth))
+        val prev_in = Input(new Entry(dataWidth, rankWidth))
+        val output_out = Output(new Entry(dataWidth, rankWidth))
+        val cmp_out = Output(Bool())
+    })
 
-  val entry = RegInit(Entry.default(dataWidth, rankWidth))
-  io.output := entry
+    val entry = RegInit(Entry.default(dataWidth, rankWidth))
+    io.output_out := entry
 
-  // 高优先级的block(rank较小的)放在右边
-  when(io.read && !io.write) {
-    // dequeue操作,entry依次右移
-    entry := io.left
-  }.elsewhen(!io.read && io.write) { // enqueue操作
-    when(entry.rank > io.new_entry.rank) { // 当前优先级小于新entry优先级
-      when(io.new_entry.rank > io.right.rank) { // entry > new_entry > right
-        entry := io.new_entry // 当前位置就是新entry最终位置
-      }.otherwise {
-        entry := io.right // 新entry在右侧插入,新entry左侧元素左移
-      }
-    } // 当前优先级高于新entry优先级,不移动
-  }
+    io.cmp_out := Mux(entry.rank > io.new_entry_in.rank, true.B, false.B)
+
+    // 高优先级的block(rank较小的)放在右边
+    when(io.read_in && !io.write_in) {
+        // dequeue操作,entry依次右移
+        entry := io.next_in
+        }.elsewhen(!io.read_in && io.write_in) { // enqueue操作
+            when(io.cmp_out) { // 当前优先级小于新entry优先级
+                entry := Mux(io.cmp_prev_in, io.prev_in, io.new_entry_in)
+            } // 当前优先级高于新entry优先级,不移动
+        }.elsewhen(io.read_in && io.write_in) {
+            when(!io.cmp_out) {
+                entry := Mux(io.cmp_next_in, io.new_entry_in, io.next_in)
+            }
+        }
 }
 
-class ShiftRegister(dataWidth: Int, rankWidth: Int, blocksNum: Int)
+class ShiftRegister(val dataWidth: Int, val rankWidth: Int, val blocksNum: Int, debug: Boolean = false)
     extends Module {
-  val io = IO(new Bundle {
-    val read = Input(Bool())
-    val write = Input(Bool())
-    val new_entry = Input(new Entry(dataWidth, rankWidth))
-    val output = Output(new Entry(dataWidth, rankWidth))
-  })
+    val io = IO(new Bundle {
+        val read_in = Input(Bool())
+        val write_in = Input(Bool())
+        val new_entry_in = Input(new Entry(dataWidth, rankWidth))
+        val output_out = Output(new Entry(dataWidth, rankWidth))
+        // 添加debug可选端口
+        val dbgPort = if(debug) Some(Output(Vec(blocksNum, new Entry(dataWidth, rankWidth)))) else None
+    })
 
-  val blocks = Seq.fill(blocksNum)(Module(new Block(dataWidth, rankWidth)))
-  blocks(0).io.right := Entry.default(dataWidth, rankWidth)
-  // 0th block右侧entry优先级设为最高
-  blocks(0).io.right.rank := 0.U
+    val blocks = Seq.fill(blocksNum)(Module(new Block(dataWidth, rankWidth)))
+    val highest = RegNext(blocks(0).io.output_out)
+    
+    blocks(0).io.cmp_prev_in := false.B
+    blocks(0).io.prev_in := Entry.default(dataWidth, rankWidth)
+    blocks(0).io.prev_in.rank := 0.U
+    
+    blocks(blocksNum - 1).io.next_in := Entry.default(dataWidth, rankWidth)
+    blocks(blocksNum - 1).io.cmp_next_in := true.B
 
-  // 最左block的左侧entry优先级初始化为最低
-  blocks(blocksNum - 1).io.left := Entry.default(dataWidth, rankWidth)
-
-  for (i <- 0 until blocksNum) {
-    blocks(i).io.read := io.read
-    blocks(i).io.write := io.write
-    blocks(i).io.new_entry := io.new_entry
-    if (i != 0) {
-      blocks(i).io.right := blocks(i - 1).io.output
+    for (i <- 0 until blocksNum) {
+        blocks(i).io.read_in := io.read_in
+        blocks(i).io.write_in := io.write_in
+        blocks(i).io.new_entry_in := io.new_entry_in
+        if (i != 0) {
+            blocks(i).io.prev_in := blocks(i - 1).io.output_out
+            blocks(i).io.cmp_prev_in := blocks(i - 1).io.cmp_out
+        }
+        if (i != blocksNum - 1) {
+            blocks(i).io.next_in := blocks(i + 1).io.output_out
+            blocks(i).io.cmp_next_in := blocks(i + 1).io.cmp_out
+        }
+        
+        if(debug) {
+            io.dbgPort.get(i) := blocks(i).io.output_out
+        }
     }
-    if (i != blocksNum - 1) {
-      blocks(i).io.left := blocks(i + 1).io.output
-    }
-  }
 
-  io.output := blocks(0).io.output
+    when(io.read_in && io.write_in) {
+        when(io.new_entry_in.rank < highest.rank) {
+            io.output_out := io.new_entry_in
+        }.otherwise {
+            io.output_out := highest
+        }
+    }.otherwise {
+        io.output_out := blocks(0).io.output_out
+    }
 
 }
+
--- src/test/scala/ShiftRegister/SRtest.scala
+++ src/test/scala/ShiftRegister/SRtest.scala
@@ -3,78 +3,66 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class SRtest extends AnyFlatSpec with ChiselScalatestTester {
+  def debugPrint(dut: ShiftRegister, tag: String): Unit = {
+    val entries = dut.io.dbgPort.get.map(_.rank.peek().litValue)
+    println(f"${tag}%-8s: ${entries.mkString("[", ", ", "]")}")
+  }
+
   def enqueue(dut: ShiftRegister, rank: Int): Unit = {
-    dut.io.read.poke(false.B)
-    dut.io.write.poke(true.B)
-    dut.io.new_entry.rank.poke(rank.U)
+    dut.io.read_in.poke(false.B)
+    dut.io.write_in.poke(true.B)
+    dut.io.new_entry_in.rank.poke(rank.U)
     dut.clock.step()
+    debugPrint(dut, s"enq(${rank})")
   }
 
   def dequeue(dut: ShiftRegister): Unit = {
-    dut.io.read.poke(true.B)
-    dut.io.write.poke(false.B)
+    dut.io.read_in.poke(true.B)
+    dut.io.write_in.poke(false.B)
     dut.clock.step()
+    debugPrint(dut, "deq")
   }
 
-  def hazard(dut: ShiftRegister) = {
-    dut.io.read.poke(true.B)
-    dut.io.write.poke(true.B)
-    dut.io.new_entry.rank.poke(1.U)
+  def replace(dut: ShiftRegister, rank: Int): Unit = {
+    dut.io.read_in.poke(true.B)
+    dut.io.write_in.poke(true.B)
+    dut.io.new_entry_in.rank.poke(rank.U)
     dut.clock.step()
+    debugPrint(dut, s"rep(${rank})")
   }
 
+  // 原测试代码基础上增加调试输出调用
   "ShiftRegister" should "pass" in {
-    test(new ShiftRegister(0, 4, 4)) { dut =>
-      // null, 默认优先级是b1111
-      dut.io.output.rank.expect(15.U)
+    test(new ShiftRegister(0, 4, 4, debug = true)) { dut =>  // 添加debug参数
+      debugPrint(dut, "Init")     // 初始状态输出
+      dut.io.output_out.rank.expect(15.U)
 
-      // 1
       enqueue(dut, 1)
-      dut.io.output.rank.expect(1.U)
+      dut.io.output_out.rank.expect(1.U)
 
-      // 4, 1
       enqueue(dut, 4)
-      dut.io.output.rank.expect(1.U)
+      dut.io.output_out.rank.expect(1.U)
 
-      // 4
       dequeue(dut)
-      dut.io.output.rank.expect(4.U)
+      dut.io.output_out.rank.expect(4.U)
 
-      // 5, 4
       enqueue(dut, 5)
-      dut.io.output.rank.expect(4.U)
+      dut.io.output_out.rank.expect(4.U)
 
-      // 5, 4, 2
       enqueue(dut, 2)
-      dut.io.output.rank.expect(2.U)
+      dut.io.output_out.rank.expect(2.U)
 
-      // 5, 4, 2
-      hazard(dut)
-      dut.io.output.rank.expect(2.U)
+      enqueue(dut, 7)
+      dut.io.output_out.rank.expect(2.U)
 
-      // 5, 4, 2, 1
-      enqueue(dut, 1)
-      dut.io.output.rank.expect(1.U)
+      replace(dut, 3)
+      dut.io.output_out.rank.expect(2.U)
 
-      // 4, 2, 1, 0
-      enqueue(dut, 0)
-      dut.io.output.rank.expect(0.U)
+      replace(dut, 6)
+      dut.io.output_out.rank.expect(3.U)
 
-      // 4, 2, 1
-      dequeue(dut)
-      dut.io.output.rank.expect(1.U)
-
-      // 4, 2
-      dequeue(dut)
-      dut.io.output.rank.expect(2.U)
-
-      // 4
-      dequeue(dut)
-      dut.io.output.rank.expect(4.U)
-
-      // null, 默认优先级是b1111
-      dequeue(dut)
-      dut.io.output.rank.expect(15.U)
+      replace(dut, 1)
+      dut.io.output_out.rank.expect(1.U)
     }
   }
 }
